09da10c7ccc275956301cee8e2356e4f85d5c1a9
Amithsk <amithsebkanatt@gmail.com>
2016-09-04 08:57:19 +0530

    Major revamp in the design  created separate function for different use case   1. Water sensor   2. Relay part Will  trigger call to each use case

--- MainFile.py
+++ MainFile.py
@@ -8,45 +8,13 @@
 from time import strftime,localtime,sleep
 from datetime import datetime
 import RPi.GPIO as GPIO
+from WaterSensor import waterLevel
+from Relay import relayTrigger
 
-def relayControl():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(2,GPIO.OUT)
-  GPIO.output(2,GPIO.HIGH)
-  sleep(30)
-  GPIO.output(2,GPIO.LOW)
-  sleep(5)
-  GPIO.cleanup()
  
 def diffTimeCalc(start,end):
   diff = datetime.strptime(start,FMT)-datetime.strptime(end,FMT)
   return diff  
-
-def waterLevel():
-  GPIO.setmode(GPIO.BCM)
-  TRIG = 23
-  ECHO = 25
-
-#Init pin  
-  GPIO.setup(TRIG,GPIO.OUT)
-  GPIO.setup(ECHO,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-# Trigger the sensor
-  GPIO.output(TRIG,False)
-  time.sleep(2)
-  GPIO.output(TRIG,True)
-  time.sleep(0.00001)
-  GPIO.output(TRIG,False)
-#Policing the state if ECHO
-  while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
-   
-  while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
-  pulse_duration = pulse_end =pulse_start
-  distance = pulse_duration*17150
-  distance = round(distance,2)
-  print "Distance",distance
-  GPIO.cleanup()
 
 #Time format
 FMT = '%H:%M:%S'
@@ -66,7 +34,7 @@ while True:
   startTime = datetime.now().strftime('%H:%M:%S')
   print('Waking up',startTime)
   waterLevel()
-  relayControl()
+  relayTrigger()
   endTime=datetime.now().strftime('%H:%M:%S')
   tdelta= diffTimeCalc(startTime,endTime)
   tdelta = 300-(tdelta.total_seconds())
