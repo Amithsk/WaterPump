04eb17a0af73f0ef4e372256ed99fd106bea79ad
Amithsk <amithsebkanatt@gmail.com>
2016-09-04 08:25:47 +0530

    Updated the file for better handling Spliting functionality from main file

--- WaterSensor.py
+++ WaterSensor.py
@@ -2,36 +2,39 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-TRIG =23
-ECHO =25
+def waterLevel():
+  waterLevelCheck()
 
-#Init pin
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+def waterLevelCheck():
+  GPIO.setmode(GPIO.BCM)
+  TRIG =23
+  ECHO =25
 
-
-
-#Trigger the sensor
-GPIO.output(TRIG,False)
-time.sleep(2)
+  #Init pin
+  GPIO.setup(TRIG,GPIO.OUT)
+  GPIO.setup(ECHO,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
 
-#Activate Trigger
-GPIO.output(TRIG,True)
-time.sleep(0.00001)
-GPIO.output(TRIG,False)
+  #Trigger the sensor
+  GPIO.output(TRIG,False)
+  time.sleep(2)
 
 
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+  #Activate Trigger
+  GPIO.output(TRIG,True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG,False)
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
+
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
 
 #Calculate distance
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration*17150
-distance =round(distance,2)
+  pulse_duration = pulse_end - pulse_start
+  distance = pulse_duration*17150
+  distance =round(distance,2)
 
-GPIO.cleanup()
+  GPIO.cleanup()
