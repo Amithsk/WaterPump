36424fc7a2343af977903282e5ef6ac6f7a4c19a
Amithsk <amithsebkanatt@gmail.com>
2016-09-17 10:23:49 +0530

    Added new file for all the file processing stuff

---
+++ FileIO.py
@@ -0,0 +1,33 @@
+#This file will have
+#1/ To check if the file exist
+#2/ To write the content into file
+#3/ To create the file
+#4/ To read the content of the file
+
+import sys
+
+def isFilePresent():
+  print("Inside is file present check")
+
+
+
+def readFileContent():
+  print("Inside read file usecase")
+
+
+
+def writeFileContent():
+  print("Insde write file usecase")
+
+
+
+def createFile():
+  print ("Inside create file usecase")
+
+
+
+def deleteFile():
+  print("Inside delete file usecase")
+
+
+createFile()
