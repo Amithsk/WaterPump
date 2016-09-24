df8f7ecb34c931bad7b2e791f5a47804b045a1ab
Amithsk <amithsebkanatt@gmail.com>
2016-09-24 10:51:46 +0530

    Updated the script to access the location and weather information

--- Weather.py
+++ Weather.py
@@ -1,15 +1,31 @@
 import pyowm
 import json
+import requests
+import json
 
-owm = pyowm.OWM('8a41803eb0d905c467564e6e4822d7d2')  # You MUST provide a valid API key
+def retrieveLocation():
+  url = 'http://freegeoip.net/json'
+  Res = requests.get(url)
+  Temp= json.loads(Res.text)
+  Location =''.join([Temp['city'],',',Temp['country_code']])
+  return Location
 
+
+def retrieveWeatherdetails():
+#Retrieve location information
+ Location = retrieveLocation()
+
+#Retrieve weather information
+ owm = pyowm.OWM('8a41803eb0d905c467564e6e4822d7d2')  # You MUST provide a valid API key
 
 # Search for current weather 
-observation = owm.weather_at_place('Bangalore,IN')
-w = observation.get_weather()
+ observation = owm.weather_at_place('Location')
+ JSON = observation.to_JSON()
+ JS = json.loads(JSON)
+ return JS['Weather']['status'],JS['Weather']['humidity'],JS['Weather']['temperature']['temp_max'],JS['Weather']['temperature']['temp_min']
 
-JSON = observation.to_JSON()
-JS = json.loads(JSON)
-print (JS['Weather']['status'])
-print (JS['Weather']['humidity'])
-print (JS['Weather']['temperature']['temp_max'])
+
+
+def weatherUpdate():
+  retrieveWeatherdetails()
+
